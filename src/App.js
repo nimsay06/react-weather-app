5e4b5275969378f65286b507340b0c7a56cd6258
nimsay06 <ytercenio@yahoo.co.uk>
2022-02-24 14:07:32 +0000

    added github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Yasmin Tercenio
+        <a
+          href="https://github.com/nimsay06/react-weather-app.git"
+          target="_blank"
+        >
+          Open source code on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
